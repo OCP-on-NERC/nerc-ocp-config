7db943b05965209440af0743b7bcc2879754627a
dystewart <dystewar@redhat.com>
2022-11-23 16:12:54 -0500

    Update vault seal status script to add seal status annotation to vault statefulset
    linting

--- vault/overlays/nerc-ocp-infra/auto-unseal/scripts/check-vault-seal.sh
+++ vault/overlays/nerc-ocp-infra/auto-unseal/scripts/check-vault-seal.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -ue
 
+echo "Checking vault seal status..."
+
 oc exec -i nerc-vault-0 -- vault status --format=json > /tmp/sealed.json
-sealed=$(yq -r '.sealed')
-echo "$sealed"
+sealed=$(cat /tmp/sealed.json | yq -r '.sealed')
+echo "Sealed: $sealed"
+
+oc annotate statefulset/nerc-vault sealed=$sealed
