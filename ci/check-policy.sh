58547940b23a13e7c75d3133ed922dfa3d879e03
Lars Kellogg-Stedman <lars@redhat.com>
2026-06-09 18:53:38 -0400

    More flexible ignore logic for policy checks
    Instead of hardcoding the ignore patterns, add support for a
    `.policyignore` and a `.policyignore.local` file, both of which are lists
    of regular expressions, one per line, of paths to ignore.

--- ci/check-policy.sh
+++ ci/check-policy.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-docker run --rm -v "$PWD:/project" openpolicyagent/conftest test \
-  --all-namespaces \
-  --ignore '\.venv|/charts/' \
-  .
+IGNOREFILE=.policyignore
+IGNOREFILE_LOCAL=.policyignore.local
+
+find . \( -name '*.yaml' -o -name '*.yml' \) -print0 |
+  if [ -f "$IGNOREFILE" ]; then grep -zvFf "$IGNOREFILE"; else cat; fi |
+  if [ -f "$IGNOREFILE_LOCAL" ]; then grep -zvFf "$IGNOREFILE_LOCAL"; else cat; fi |
+  xargs -0 docker run --rm -v "$PWD:/project" openpolicyagent/conftest test \
+    --all-namespaces
